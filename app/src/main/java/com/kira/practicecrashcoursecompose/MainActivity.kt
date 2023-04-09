07d6d0b4fc11bc3886cdd29c07690f49f6f12975
juliusv-metromart <kira@metromart.com>
2023-04-09 22:48:54 +0800

    basic remember and mutable state of example

--- app/src/main/java/com/kira/practicecrashcoursecompose/MainActivity.kt
+++ app/src/main/java/com/kira/practicecrashcoursecompose/MainActivity.kt
@@ -3,17 +3,18 @@ package com.kira.practicecrashcoursecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kira.practicecrashcoursecompose.ui.theme.PracticeCrashCourseComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,32 +22,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeCrashCourseComposeTheme {
-                Greeting("Android")
+                var count by remember {
+                    mutableStateOf(0)
+                }
+
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        fontSize = 30.sp,
+                        text = count.toString()
+                    )
+                    Button(onClick = {
+                        count++
+                    }) {
+                        Text(text = "Click me! $count")
+                    }
+                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(count = 10) {index ->
-            Image(painter = painterResource(
-                id = R.drawable.ic_launcher_foreground),
-                contentDescription = null,
-                modifier = Modifier.background(Color.Red).size(100.dp)
-            )
-        }
-    }
 
-
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PracticeCrashCourseComposeTheme {
-        Greeting("Android")
-    }
-}
