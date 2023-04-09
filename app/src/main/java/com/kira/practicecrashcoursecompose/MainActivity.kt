0e760a865c42d7ad2fe4d7eda58fdf9868c4a6d8
juliusv-metromart <kira@metromart.com>
2023-04-09 23:21:49 +0800

    simple adding of data in textfield and putting in lazy column

--- app/src/main/java/com/kira/practicecrashcoursecompose/MainActivity.kt
+++ app/src/main/java/com/kira/practicecrashcoursecompose/MainActivity.kt
@@ -3,18 +3,25 @@ package com.kira.practicecrashcoursecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
 import com.kira.practicecrashcoursecompose.ui.theme.PracticeCrashCourseComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,23 +29,48 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PracticeCrashCourseComposeTheme {
-                var count by remember {
-                    mutableStateOf(0)
+                var name by remember {
+                    mutableStateOf("")
                 }
-
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(
-                        fontSize = 30.sp,
-                        text = count.toString()
-                    )
-                    Button(onClick = {
-                        count++
-                    }) {
-                        Text(text = "Click me! $count")
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)) {
+                   Row(
+                       modifier = Modifier.fillMaxWidth()
+                   ) {
+                       OutlinedTextField(
+                           value = name,
+                           onValueChange = { text ->
+                               name = text
+                           },
+                           modifier = Modifier.weight(1f)
+                       )
+                       Spacer(modifier = Modifier.width(16.dp))
+                       Button(
+                           onClick = {
+                               if (name.isNotBlank()) {
+                                   names = names +name
+                                   name = ""
+                               }
+                           }
+                       ) {
+                           Text(text = "Add")
+                       }
+                   }
+                    LazyColumn {
+                        items(names) { currentName ->
+                            Text(
+                                text = currentName,
+                                modifier =
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                            Divider()
+                        }
                     }
                 }
             }
